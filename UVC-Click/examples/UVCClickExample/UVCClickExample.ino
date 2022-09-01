227e6674439ae20778e8903f2d931b243a2be7db
Sarah Cartwright <sarah@untamed.zone>
2022-09-01 13:29:01 +0100

    Add files via upload

---
+++ UVC-Click/examples/UVCClickExample/UVCClickExample.ino
@@ -0,0 +1,33 @@
+
+#include <UVC_Click.h>
+
+UVC_Click sensor;
+
+void setup() {
+	Serial.begin(112500);
+	sensor.begin(&Wire, 21, 22); // SDA, SDL
+	delay(2000);
+	Serial.println("Reading UVC levels...");
+}
+
+void loop() {
+	char info[40];
+	uint16_t raw_data;
+	float voltage;
+	float power;
+
+	raw_data = sensor.readRawUVCData();
+	sprintf(info, "Raw data: %d\r\n", raw_data);
+	Serial.print(info);
+
+	voltage = sensor.getVoltage();
+	sprintf(info, "Voltage: %.4f mV\r\n", voltage);
+	Serial.print(info);
+
+	power = sensor.calculateUVCPower(voltage);
+	sprintf(info, "Power: %.4f mW/cm2\r\n", power);
+	Serial.print(info);
+
+	Serial.println("");
+	delay(1000);
+}
