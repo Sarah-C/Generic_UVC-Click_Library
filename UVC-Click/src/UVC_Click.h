0308b036de2cf8cc6c6196d565333ea890ee3d7f
Sarah Cartwright <sarah@untamed.zone>
2022-09-01 13:33:31 +0100

    Add files via upload

--- UVC-Click/src/UVC_Click.h
+++ UVC-Click/src/UVC_Click.h
@@ -9,20 +9,20 @@
 #define DEVICE_I2C_ADDR 0x4D
 
 class UVC_Click {
-   private:
-    TwoWire *_wire;
-    uint8_t _sda;
-    uint8_t _scl;
-    uint32_t _freq;
-    uint8_t _addr;
-    void writeByte(byte cmd);
-    void writeBytes(uint8_t *buffer, size_t size);
-    void readBytes(uint8_t *buffer, size_t size);
+private:
+	TwoWire *_wire;
+	uint8_t _sda;
+	uint8_t _scl;
+	uint32_t _freq;
+	uint8_t _addr;
+	void writeByte(byte cmd);
+	void writeBytes(uint8_t *buffer, size_t size);
+	void readBytes(uint8_t *buffer, size_t size);
 
-   public:
-    UVC_Click(uint8_t addr = 0x4D);
-    void begin(TwoWire *wire = &Wire, uint8_t sda = SDA, uint8_t scl = SCL,
-               uint32_t freq = 4000000UL);
+public:
+	UVC_Click(uint8_t addr = 0x4D);
+	void begin(TwoWire *wire = &Wire, uint8_t sda = SDA, uint8_t scl = SCL,
+				uint32_t freq = 4000000UL);
 
 	uint16_t readRawUVCData(); // 12 bits data
 	float getVoltage(); 
