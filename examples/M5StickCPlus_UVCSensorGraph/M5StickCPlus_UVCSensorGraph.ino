f129fbd0789ac9a3b9365db0052f12cb1e8def7e
Sarah Cartwright <sarah@untamed.zone>
2022-09-08 14:31:36 +0100

    Update M5StickCPlus_UVCSensorGraph.ino

--- examples/M5StickCPlus_UVCSensorGraph/M5StickCPlus_UVCSensorGraph.ino
+++ examples/M5StickCPlus_UVCSensorGraph/M5StickCPlus_UVCSensorGraph.ino
@@ -1,3 +1,4 @@
+
 /*
  * 
  * M5StickC Plus sketch for Arduino that uses the GUVC-T21GH sensor
@@ -13,28 +14,40 @@
 
 UVC_Click sensor;
 TFT_eSprite disbuff = TFT_eSprite(&M5.Lcd);
-
+char textBuffer[40]; // Place for sprintf
+  
 int graphColor = 0;
 int textColor = TFT_GREEN;
 int updateSpeed = 250; // millisecond delay
 
 void setup() {
   M5.begin();
+
+  M5.Axp.EnableCoulombcounter(); 
+  
   Serial.begin(115200);
-	pinMode(25, INPUT);
-	pinMode(26, INPUT);
-	gpio_pulldown_dis(GPIO_NUM_36);
-	gpio_pullup_dis(GPIO_NUM_36);  
+  pinMode(25, INPUT);
+  pinMode(26, INPUT);
+  gpio_pulldown_dis(GPIO_NUM_36);
+  gpio_pullup_dis(GPIO_NUM_36);  
   sensor.begin(&Wire, 25, 26); // 25 = SDA, 26 = SCL
   
   M5.Lcd.setRotation(1);
   disbuff.setTextColor(textColor);
   disbuff.setTextDatum(ML_DATUM);
-  disbuff.setTextSize(2);
   disbuff.createSprite(240, 135);
   graphColor = disbuff.color565(100, 50, 50);
   disbuff.fillRect(0, 0, 240, 135, disbuff.color565(255, 0, 255));
   disbuff.pushSprite(0, 0);
+}
+
+void loop() {
+  disbuff.fillSprite(TFT_BLACK);
+  getAndDrawWaveform();
+  getAndDrawLightValues();
+  getAndDrawBatteryValues();
+  disbuff.pushSprite(0, 0);
+  delay(updateSpeed);
 }
 
 int getSampleAverage(int reads, int wait) {
@@ -46,14 +59,6 @@ int getSampleAverage(int reads, int wait) {
   return total / reads;
 }
 
-void loop() {
-  disbuff.fillSprite(TFT_BLACK);
-  getAndDrawWaveform();
-  getAndDrawTextValues();
-  disbuff.pushSprite(0, 0);
-  delay(updateSpeed);
-}
-
 void getAndDrawWaveform() {
   double sample = 0;
   int y = 0;
@@ -61,10 +66,10 @@ void getAndDrawWaveform() {
   uint16_t samples[240];
   for (uint8_t x = 0; x < 240; x++) {
     samples[x] = sensor.readRawUVCData();
-    // delayMicroseconds(1);
+    delayMicroseconds(250);
   }
   sample = (double) samples[0];
-  oldy = (int)((sample * 134.0f) / 4095.0f);
+  oldy = (int)((sample * 134.0) / 4095.0);
   for (uint8_t x = 1; x < 240; x++) {
     sample = (double) samples[x];
     y = (int)((sample * 134.0) / 4095.0);
@@ -73,26 +78,38 @@ void getAndDrawWaveform() {
   }
 }
 
-void getAndDrawTextValues() {
-  int stepY = 30; // The the text seperation
+void getAndDrawBatteryValues(){
+  disbuff.setTextSize(1);
+
+  // 3.4v Flat battery, 4.2v Full battery.
+  float bv = M5.Axp.GetBatVoltage() - 3.4f;
+  bv = (bv / 0.8f) * 100.0f;
+  
+  sprintf(textBuffer, "Bat: %.3f %%", bv);
+  disbuff.drawString(textBuffer, 150, 12);
+}
+
+void getAndDrawLightValues() {
+  int stepY = 25; // The the text seperation
   int startY = 12;
 
   int rawAnalog = 0;
   float voltage = 0;
   float mwcm = 0;
 
-  char info[40]; // Place for sprintf
-
-  rawAnalog = getSampleAverage(100, 1);// Reads to average over, wait between them.
+  rawAnalog = getSampleAverage(100, 1) - 1;// Reads to average over, wait between them.
   voltage = (rawAnalog * 3.3f ) / (4095.0f); // (raw / 4095) = 0..1,  3.3 * 0..1 = volts
   mwcm = voltage * 0.355f;
+  
+  disbuff.setTextSize(2);
+  
+  sprintf(textBuffer, "Raw: %d", rawAnalog);
+  disbuff.drawString(textBuffer, 10, startY);
 
-  sprintf(info, "Raw: %d", rawAnalog);
-  disbuff.drawString(info, 10, startY);
+  sprintf(textBuffer, "Volts: %.2f", voltage);
+  disbuff.drawString(textBuffer, 10, startY + stepY);
 
-  sprintf(info, "Volts: %.2f", voltage);
-  disbuff.drawString(info, 10, startY + stepY);
-
-  sprintf(info, "mW/cm^2: %.2f", mwcm);
-  disbuff.drawString(info, 10, startY + (stepY * 2));
+  sprintf(textBuffer, "mW/cm^2: %.4f", mwcm);
+  disbuff.drawString(textBuffer, 10, startY + (stepY * 2));
 }
+
